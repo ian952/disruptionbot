fce0432cee6ee92ee78856547bea424bd90d9e02
ian952 <ianhu952@hotmail.com>
2017-03-08 22:19:31 -0800

    use env for auth token

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 
-# mybot
+# Disruption Bot
 
-`mybot` is an working Slack bot written in Go. Fork it and use it to build
-your very own cool Slack bot!
-
-Check the [blog post](https://www.opsdash.com/blog/slack-bot-in-golang.html)
-for a description of mybot internals.
-
-Follow us on Twitter today! [@therapidloop](https://twitter.com/therapidloop)
+`Disruption Bot` is a Slack bot written in Go. It will respond to a specified sequence of messages!
--- mybot.go
+++ mybot.go
@@ -1,33 +1,9 @@
-/*
-
-mybot - Illustrative Slack bot in Go
-
-Copyright (c) 2015 RapidLoop
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 package main
 
 import (
 	"encoding/csv"
 	"fmt"
+	"github.com/jpfuentes2/go-env"
 	"log"
 	"net/http"
 	"os"
@@ -35,13 +11,11 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "usage: mybot slack-bot-token\n")
-		os.Exit(1)
-	}
+	env.ReadEnv("./.env")
 
 	// start a websocket-based Real Time API session
-	ws, id := slackConnect(os.Args[1])
+	fmt.Printf("Auth token: '%s'\n", os.Getenv("AUTH_TOKEN"))
+	ws, id := slackConnect(os.Getenv("AUTH_TOKEN"))
 	fmt.Println("mybot ready, ^C exits")
 
 	for {
